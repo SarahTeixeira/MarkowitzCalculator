9df7d4cec827064c03dbd2421ecfaddd609ef251
PedroHenrique31 <pedrohcaraujo1@hotmail.com>
2019-09-26 18:13:00 -0300

    Detalhamento de como funciona o modelo de Markowitz.

--- wallet.R
+++ wallet.R
@@ -1,10 +1,15 @@
 ####################################################################################
-#####     Calculadora de Markowitz
-#####   Projeto de  calculadora em R para realizar a otimizacao de portfolio 
+#####                          Calculadora de Markowitz
+#####     Projeto de  calculadora em R para realizar a otimizacao de portfolio 
 ##### seguindo o modelo de proposto por Markowitz em 1952, para isso usamos os 
 ##### dados de Petrobras(PETR4), e o indice bovespa (BOVA11).
 ##### Os dados foram importados em formato .csv do yahoo financas, e correspondem 
-##### ao periodo de 26/09/2018 até 26/09/2019(diario), para esse calculo a funcao
+##### ao periodo de 26/09/2018 até 26/09/2019(diario).
+#####     A modelagem se baseia em três pilares (MARKOWITZ, 1952): 
+##### 1) retorno esperado para a carteira; 
+##### 2) a proporção com que os ativos comporão a carteira; 
+##### 3) a variância da carteira que representará o risco incorrido para a mesma
+#####     para esse calculo a funcao
 #####  deve calcular:
 #####     1- o rendimento diario de cada ativo;
 #####     2- A media do rendimento de cada ativo;
