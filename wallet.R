8be020373b41eafd6fe7c0ba8713bcf31649081a
PedroHenrique31 <pedrohcaraujo1@hotmail.com>
2019-09-26 18:09:51 -0300

    Breve explanacao de objetivos.

--- wallet.R
+++ wallet.R
@@ -1,4 +1,18 @@
-#####Calcladora Markowitz
+####################################################################################
+#####     Calculadora de Markowitz
+#####   Projeto de  calculadora em R para realizar a otimizacao de portfolio 
+##### seguindo o modelo de proposto por Markowitz em 1952, para isso usamos os 
+##### dados de Petrobras(PETR4), e o indice bovespa (BOVA11).
+##### Os dados foram importados em formato .csv do yahoo financas, e correspondem 
+##### ao periodo de 26/09/2018 até 26/09/2019(diario), para esse calculo a funcao
+#####  deve calcular:
+#####     1- o rendimento diario de cada ativo;
+#####     2- A media do rendimento de cada ativo;
+#####     3- A variancia deste rendimento;
+#####     4- A covariancia dos ativos;
+#####     5- A correlação entre eles.
+####################################################################################
+
 #importa arquivos de dados Petrobras
 PETR4.SA <- read.csv("C:/Users/pedro/RStudioProjects/Markowitz/Markowitz_wallet/PETR4.SA.csv")
 View(PETR4.SA)
