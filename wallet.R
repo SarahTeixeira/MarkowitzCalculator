a5a4dacf568cf6738e0ff5ff057fecefa7e3b794
PedroHenrique31 <pedrohcaraujo1@hotmail.com>
2019-09-26 17:59:32 -0300

    Primeiro commit do projeto de criar uma calculadora de markowitz.

---
+++ wallet.R
@@ -0,0 +1,7 @@
+#####Calcladora Markowitz
+#importa arquivos de dados Petrobras
+PETR4.SA <- read.csv("C:/Users/pedro/RStudioProjects/Markowitz/Markowitz_wallet/PETR4.SA.csv")
+View(PETR4.SA)
+#importa arquivos Bovespa
+BVSPA <- read.csv("C:/Users/pedro/RStudioProjects/Markowitz/Markowitz_wallet/^BVSP.csv")
+View(BVSPA)
